f7c43612ef1871a8b630d0e50da153cbe164d757
unknown <mkhanhaa@gmail.com>
2022-07-25 18:03:57 +0700

    update 25 month 7

---
+++ ss2_design_databse/exercise/e2/product_manager.sql
@@ -0,0 +1,26 @@
+ create database if not exists product_manager_1;
+ use product_manager_1;
+ create table customer(
+	cID int primary key auto_increment,
+    cName varchar(50),
+    cAge int
+ );
+ create table `order` (
+	oID int auto_increment primary key,
+    cID int,
+    oDate date,
+    oTotaPrice double,
+    foreign key (cID) references customer(cID)
+ );
+ create table product(
+	pID int auto_increment primary key,
+    pName varchar(50),
+    pPrice double
+ );
+ create table orderDetail(
+	oID int,
+    pID int,
+    OdQTY varchar(50),
+    foreign key(oID) references `order`(oID),
+    foreign key(pID) references product(pID)
+ );
